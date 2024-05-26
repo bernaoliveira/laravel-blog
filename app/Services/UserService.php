6ab7da4ccddd6885f237c11a585a4da29574c34f
Danil Gilman <d.gilman@skycoach.gg>
2024-05-26 23:27:47 +0500

    fix: user password hashing

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,11 +4,13 @@ namespace App\Services;
 
 use App\Http\Requests\RegistrationPostRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserService
 {
     public function create(RegistrationPostRequest $request)
     {
-        return User::create($request->safe()->only('name', 'email', 'password'));
+        $password = Hash::make($request->input('password'));
+        return User::create($request->safe()->merge(['password' => $password])->only(['name', 'email', 'password']));
     }
 }

2a2fcf6009831c8b01c89eb9648232851567dda4
Danil Gilman <d.gilman@skycoach.gg>
2024-06-02 23:21:26 +0500

    feat: categories filters

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Filters\PaginationFilter;
+use App\Filters\TextLikeFilter;
+use App\Traits\FilterableTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -11,6 +14,7 @@ class Category extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use FilterableTrait;
 
     public const
         FIELD_SLUG = 'slug',
@@ -19,6 +23,14 @@ class Category extends Model
     protected $fillable = [
         self::FIELD_SLUG,
         self::FIELD_NAME,
+    ];
+
+    public static array $filters = [
+        PaginationFilter::class => [],
+        TextLikeFilter::class => [
+            'name' => self::FIELD_NAME,
+            'column' => self::FIELD_NAME,
+        ],
     ];
 
     public function articles(): BelongsToMany

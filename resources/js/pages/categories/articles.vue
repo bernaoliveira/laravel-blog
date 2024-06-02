60a8b015bc605a06a4014d8048b6092703c0c0fb
Danil Gilman <d.gilman@skycoach.gg>
2024-06-02 23:39:19 +0500

    feat: categories' articles

---
+++ resources/js/pages/categories/articles.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import ArticleCard from "../../components/ArticleCard.vue";
+
+const articles = ref([]);
+const route = useRoute();
+
+const fetchCategoryArticles = async () => {
+    try {
+        const params = new URLSearchParams({
+            with_relation_category: true,
+            with_relation_user: true,
+        });
+
+        const response = await fetch(`http://localhost/api/categories/${route.params.slug}/articles?${params}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        articles.value = (await response.json()).result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+onMounted(async () => {
+    await fetchCategoryArticles();
+});
+</script>
+
+<template>
+    <ArticleCard
+        v-for="article in articles"
+        :key="article.id"
+        :article="article"
+        class="mb-4"
+    />
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -5,6 +5,11 @@ const routes = [
         path: "/",
         component: () => import("./pages/articles/index.vue"),
     },
+    {
+        name: "categoriesArticles",
+        path: "/categories/:slug",
+        component: () => import("./pages/categories/articles.vue"),
+    }
 ];
 
 export default createRouter({
